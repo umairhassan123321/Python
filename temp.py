9afa50f2d56786135f6f1ea9b1bf7c9b4f8f0d61
Umair <Umair@gmail.com>
2025-09-29 07:24:49 -0700

    29-9-2025

--- temp.py
+++ temp.py
@@ -1,1 +1,9 @@
-print("Hello world")
+print ("HELLO WORLD")
+print ("i am Umair Hassan")
+print (4+4)
+# hello kamil
+# please donot remove this file
+# umair is the aurthur
+print ("my name is umair")
+print ("my brother name is kamil",6,6,sep="~",end="009\n")
+print (7+8) 
